8261afe5240bc5f47fe63324e5021a40ec87132e
Dylan Beattie <dylan@dylanbeattie.net>
2021-11-25 11:10:36 +0000

    Add GraphQL support for vehicle queries.

---
+++ dotnet/Autobarn.Website/GraphQL/GraphTypes/ManufacturerGraphType.cs
@@ -0,0 +1,14 @@
+// Autobarn.Website/GraphQL/GraphTypes/VehicleGraphType.cs
+
+using Autobarn.Data.Entities;
+using GraphQL.Types;
+
+namespace Autobarn.Website.GraphQL.GraphTypes
+{
+    public sealed class ManufacturerGraphType : ObjectGraphType<Manufacturer> {
+		public ManufacturerGraphType() {
+			Name = "manufacturer";
+			Field(c => c.Name).Description("The name of the manufacturer, e.g. Tesla, Volkswagen, Ford");
+		}
+	}
+}

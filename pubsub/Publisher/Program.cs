dcb3563ad4cf8ba640c89b90dcbf6b7a5556424b
Dylan Beattie <dylan@dylanbeattie.net>
2021-11-25 12:46:52 +0000

    Simple pubsub demo using RabbitMQ and messages

---
+++ pubsub/Publisher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using EasyNetQ;
+using Subscriber;
+
+namespace Publisher
+{
+    class Program
+    {
+        const string AMQP = "amqp://user:pass@localhost:5672";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Publisher started!");
+            var bus = RabbitHutch.CreateBus(AMQP);
+            int count = 0;
+            while (true)
+            {
+                Console.WriteLine("Press any key to publish a message:");
+                Console.ReadKey(false);
+                var message = new Message($"This is message {count++}");
+                bus.PubSub.Publish(message);
+            }
+        }
+    }
+}
---
+++ pubsub/Subscriber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using EasyNetQ;
+
+namespace Subscriber
+{
+    class Program
+    {
+        const string AMQP = "amqp://user:pass@localhost:5672";
+
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("Starting Subscriber...");
+            var bus = RabbitHutch.CreateBus(AMQP);
+            bus.PubSub.Subscribe<Message>(Guid.NewGuid().ToString(), m => {
+                Console.WriteLine($"{m.Greeting} (created at {m.CreatedAt:O}");
+            });
+            Console.WriteLine("Press Enter to quit.");
+            Console.ReadLine();
+        }
+    }
+
+    public class Message
+    {
+        public string Greeting { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public Message(string greeting)
+        {
+            this.Greeting = greeting;
+            this.CreatedAt = DateTimeOffset.Now;
+        }
+    }
+}
